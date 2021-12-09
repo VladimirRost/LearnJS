08cead7c534276baeba7546921ad534d2e7d4c60
Rost <admin@veter64.ru>
2021-12-09 21:00:02 +0400

    First greate

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let firm = 100;
+console.log(firm);
+
+//console.log(10);
+//alert(100);
+
+
+let number = 5;
+let str = `tjem`;
+let und = 100;
+console.log(und);
+
+const blll = false;
+const leftBorderWight = 1;
+console.log(number);
+number = 120;
+console.log(number); {
+    let res = 300;
+    console.log(res);
+}
+
+const objmy = {
+    name: `Имя`,
+    age: 45 ,
+    isMarried: true 
+};
+
+console.log(objmy.name);
+console.log(objmy [`age`]);
+console.log(objmy.isMarried);
+
+let arr = [`hdo.png`, `FHB.jpeg`, `ДКУ.bmp`,9];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
