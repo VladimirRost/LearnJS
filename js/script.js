2eb91df3a9d43a7214867fc329c2810eb6fcc731
Rost <admin@veter64.ru>
2021-12-10 21:37:28 +0400

    Приложение по фильмам без цикла

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 "use strict";
 
-let firm = 100;
-console.log(firm);
+//let firm = 100;
+//console.log(firm);
 
 //console.log(10);
 //alert(100);
 
 
-let number = 5;
-let str = `tjem`;
-let und = 100;
-console.log(und);
+//let number = 5;
+//let str = `tjem`;
+//let und = 100;
+//console.log(und);
 
-const blll = false;
+/*const blll = false;
 const leftBorderWight = 1;
 console.log(number);
 number = 120;
@@ -36,3 +36,46 @@ console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
 console.log(arr[3]);
+*/
+
+let numberOfFilms;
+numberOfFilms = +prompt('Сколько фильмов вы видели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+console.log(personalMovieDB.count);
+console.log(personalMovieDB.actors);
+
+let lastFilm = prompt('Какой один из последних фильмов?', ''),
+lastReitong = prompt('Оценка фильма?', ''),
+lastFilm2 = prompt('Ещё какой один из последних фильмов?', ''),
+lastReitong2 = prompt('Оценка фильма?', '');
+
+
+
+personalMovieDB.movies[lastFilm] = lastReitong;
+personalMovieDB.movies[lastFilm2] = lastReitong2;
+
+
+
+console.log (personalMovieDB);
+
+
+/*let movies = [];
+movies [0] = lastFilm;
+movies [1] = lastReitong;
+
+lastFilm = prompt('Ещё какой один из последних фильмов?', '');
+lastReitong = prompt('Оценка фильма?', '');
+movies [2] = lastFilm;
+movies [3] = lastReitong;
+
+console.log (movies[0],movies[1],movies[2],movies[3]);
+*/
+
+
